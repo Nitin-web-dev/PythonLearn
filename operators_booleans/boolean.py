11b94882db5e62e1ddf8b2250d3e5dbbff2f7544
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-19 21:20:30 +0530

    addead operators and booleans

---
+++ operators_booleans/boolean.py
@@ -0,0 +1,5 @@
+# Boolean
+# A boolean data type represents one of the two values: True or False. The use of these data types will be clear once we start using the comparison operator. The first letter T for True and F for False should be capital unlike JavaScript. Example: Boolean Values
+
+# print(True)
+# print(False)
